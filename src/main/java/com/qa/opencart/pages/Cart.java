5ea207f2f4621fd836879e6f08837ebd663df59c
Shivender singh <shivender339@gmail.com>
2024-10-05 15:58:31 +0530

    adding cart file

---
+++ src/main/java/com/qa/opencart/pages/Cart.java
@@ -0,0 +1,8 @@
+package com.qa.opencart.pages;
+
+public class Cart {
+
+	public Cart() {
+		System.out.println("Adding the cart>>>>");
+	}
+}
--- src/test/java/com/qa/opencart/tests/LoginPageTest.java
+++ src/test/java/com/qa/opencart/tests/LoginPageTest.java
@@ -6,7 +6,17 @@ import org.testng.annotations.Test;
 import com.qa.opencart.base.BaseTest;
 import com.qa.opencarts.utils.Constants;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Step;
+import io.qameta.allure.Story;
+@Epic("US:101: design the login page for demo opencart app with login, title and forgot pass and link")
+@Story("US:-100:")
 public class LoginPageTest extends BaseTest {
+	@Description("Login page title verfication.....")
+	@Severity(SeverityLevel.CRITICAL)
 	@Test(priority = 1)
 	public void loginPageTitleTest() {
 		String title = loginpage.getloginpagetitle();
@@ -15,11 +25,15 @@ public class LoginPageTest extends BaseTest {
 
 	}
 	
-	
+	@Description("forgot password link verfication.....")
+	@Severity(SeverityLevel.CRITICAL)
     @Test(priority = 2)
 	public void forgotpwdlinktest() {
 		Assert.assertTrue(loginpage.isforgotpwdLinkexist());
 	}
+	@Description("user will get login.....")
+	@Severity(SeverityLevel.BLOCKER)
+	@Step("Logging in with username: {username} and password: {maskedPassword}")
 	@Test(priority = 3)
 	public void loginTest() {
 		loginpage.dologin(prop.getProperty("Username"), prop.getProperty("Password"));
